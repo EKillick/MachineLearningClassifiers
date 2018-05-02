e5ca0588df42a87b31e1fcb2c4db80cd3f5c7446
EKillick <edwinkillick@gmail.com>
2018-05-03 00:55:31 +0100

    Model Selection Implemented, k-folds still hardcoded

--- src/machinelearningcw/EnhancedLinearPerceptron.java
+++ src/machinelearningcw/EnhancedLinearPerceptron.java
@@ -1,7 +1,10 @@
 package machinelearningcw;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Random;
 import weka.classifiers.AbstractClassifier;
+import weka.classifiers.Evaluation;
 import weka.core.AttributeStats;
 import weka.core.Instance;
 import weka.core.Instances;
@@ -22,6 +25,7 @@ public class EnhancedLinearPerceptron extends AbstractClassifier{
     private boolean modelSelection;
     private double[] attributeMeans;
     private double[] attributeStdDev;
+    private boolean useOnline;
     
     //Constructors
     
@@ -29,11 +33,22 @@ public class EnhancedLinearPerceptron extends AbstractClassifier{
      * Default constructor for a LinearPerceptron Classifier
      */
     public EnhancedLinearPerceptron(){
-        this.learningRate = 1.0; // defaults to learning rate of 1.0
+        this.learningRate = 1; // defaults to learning rate of 1.0
         this.stoppingIterations = 100; // default value of 100 iterations
         this.standardisation = true; 
         this.modelSelection = false;
         this.bias = 0.0;
+        this.useOnline = true;
+    }
+    
+    /**
+     * Constructor with default values for everything but standardisation and
+     * model selection
+     * @param std
+     * @param ms 
+     */
+    public EnhancedLinearPerceptron(boolean std, boolean ms){
+        this(1, 100, std, ms, 0, true);
     }
     
     /**
@@ -45,12 +60,13 @@ public class EnhancedLinearPerceptron extends AbstractClassifier{
      * @param bias bias that is added before classification
      */
     public EnhancedLinearPerceptron(double l, int s, boolean std, boolean ms,
-            double bias){
+            double bias, boolean useOnline){
         this.learningRate = l;
         this.stoppingIterations = s;
         this.standardisation = std;
         this.modelSelection = ms;
         this.bias = bias;
+        this.useOnline = useOnline;
     }
     
     //Accessor Methods
@@ -85,7 +101,7 @@ public class EnhancedLinearPerceptron extends AbstractClassifier{
     public int getStoppingIterations(){
         return this.stoppingIterations;
     }
-
+    
     /**
      * Builds classifier from given Instance
      * @param instances
@@ -94,13 +110,48 @@ public class EnhancedLinearPerceptron extends AbstractClassifier{
     @Override
     public void buildClassifier(Instances instances) throws Exception {
         int nonClassAttributes = (instances.numAttributes() - 1);
-        
         attributeMeans = new double[nonClassAttributes];
         attributeStdDev = new double[nonClassAttributes];
         
         weights = new double[nonClassAttributes]; // creates weights array
         Arrays.fill(weights, 1); // initialises weights to 1
         
+        if (this.modelSelection){
+            Evaluation evalOffline = new Evaluation(instances);
+            EnhancedLinearPerceptron offClassifier = new EnhancedLinearPerceptron();
+            offClassifier.useOnline = false;
+            offClassifier.buildClassifier(instances);
+            evalOffline.crossValidateModel(offClassifier, instances, 5, new Random());
+            
+            Evaluation evalOnline = new Evaluation(instances);
+            EnhancedLinearPerceptron onClassifier = new EnhancedLinearPerceptron();
+            onClassifier.buildClassifier(instances);
+            evalOnline.crossValidateModel(onClassifier, instances, 5, new Random());    
+            
+//            System.out.println("Offline Error: " + evalOffline.errorRate());
+//            System.out.println("Online Error: " + evalOnline.errorRate());
+            
+            if (evalOffline.errorRate() < evalOnline.errorRate()){
+                useOnline = false;
+            }
+        }
+        
+        if (useOnline){
+            this.buildOnlineClassifier(instances);
+        }
+        else{
+            this.buildOfflineClassifer(instances);
+        }
+        
+    }
+    
+    /**
+     * Builds an online classifier on a given instance
+     * @param instances
+     * @throws java.lang.Exception
+     */
+    public void buildOnlineClassifier(Instances instances) throws Exception{
+        int nonClassAttributes = (instances.numAttributes() - 1);
         
         for(int att = 0; att < nonClassAttributes; att++){
             //Check values are real
@@ -112,7 +163,7 @@ public class EnhancedLinearPerceptron extends AbstractClassifier{
             attributeMeans[att] = stats.mean;
             attributeStdDev[att] = stats.stdDev;
         }
-        
+
         int iterationCount = 0;
         int correctCount = 0;
         double value;
@@ -141,12 +192,72 @@ public class EnhancedLinearPerceptron extends AbstractClassifier{
                         value = instance.value(j);
                     }
                     weights[j] += (learningRate * classDiff * value);
-                    System.out.println("weight: " + j + " " + weights[j]);
                 }
-                System.out.println("\n");
                 
                 iterationCount++;
             }
+        }while(iterationCount < stoppingIterations && correctCount < instances.numInstances());
+    }
+    
+    /**
+     * Builds an offline classifier on a given instance
+     * @param instances 
+     * @throws java.lang.Exception 
+     */
+    public void buildOfflineClassifer(Instances instances) throws Exception{
+        int nonClassAttributes = (instances.numAttributes() - 1);
+        
+        double[] deltaWeights;
+        deltaWeights = new double[nonClassAttributes];
+        
+        for(int att = 0; att < nonClassAttributes; att++){
+            //Check values are real
+            if(!instances.attribute(att).isNumeric()){
+                throw new Exception("Non Real Values");
+            }
+            AttributeStats attStats = instances.attributeStats(att);
+            Stats stats = attStats.numericStats;
+            attributeMeans[att] = stats.mean;
+            attributeStdDev[att] = stats.stdDev;
+        }
+
+        int iterationCount = 0;
+        int correctCount = 0;
+        double value;
+        
+        do{
+            Arrays.fill(deltaWeights, 0);
+            for (int i = 0; i < instances.numInstances(); i++){
+                Instance instance = instances.get(i);
+                double calculated = classifyInstance(instance);
+                double actual = instance.classValue();
+                double classDiff = actual - calculated;
+//                System.out.println(calculated);
+                
+                if (classDiff == 0.0){
+                    correctCount++;
+                } 
+                else{
+                    correctCount = 0;
+                }
+                
+                //update the weights
+                for (int j = 0; j < nonClassAttributes; j++){
+                    if (standardisation){
+                    value = standardiseValue(instance.value(j), 
+                                        attributeMeans[j], attributeStdDev[j]);
+                    }
+                    else{
+                        value = instance.value(j);
+                    }
+                    deltaWeights[j] += (learningRate * classDiff * value);
+                }
+            }
+            for (int k = 0; k < nonClassAttributes; k++){
+                weights[k] += deltaWeights[k];
+            }
+//            System.out.println(weights[0] + " " + weights[1]);
+            iterationCount++;
         }while(iterationCount < stoppingIterations && correctCount < instances.numInstances());
     }
 
@@ -170,7 +281,7 @@ public class EnhancedLinearPerceptron extends AbstractClassifier{
                 else{
                     value = instnc.value(i);
             }
-       result += (weights[i] * value);
+            result += (weights[i] * value);
        }
        result += bias;
        
